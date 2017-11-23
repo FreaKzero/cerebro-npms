bf44bdfcb94e629e3265300a5b3f6c34804519a3
Thomas "Pete" Petrovic <pete@freakzero.com>
2017-11-23 17:52:14 +0100

    Several improvements

--- src/index.js
+++ src/index.js
@@ -1,33 +1,44 @@
 import debounce from 'debounce';
 import request from 'request-promise-native';
 import React from 'react';
-import Preview from './preview';
-import SubTitle from './preview/subtitle';
-import styles from './preview/styles.css';
+import Preview from './Preview';
+import Subtitle from './Subtitle';
+import styles from './Preview/styles.css';
 
 export const fn = ({ term, display, actions }) => {
   let match = term.match(new RegExp(/^npms\s*(.*)/, 'i'));
-  if (match) {
-    debounce(
-      request({
-        url: 'https://api.npms.io/v2/search',
-        qs: { q: match[1].replace(/"/g, '\\"') },
-        json: true
-      }).then(data => {
-        data.results.forEach(item => {
-          display({
-            title: item.package.name,
-            subtitle: <SubTitle item={item} />,
-            getPreview: () => (
-              <Preview item={item} actions={ actions } />
-            ),
-            onSelect: event => {
-              actions.open(item.package.links.npm);
-            }
-          })
-        });
-      })
-    , 500);
+
+  if (match && match[1]) {
+    let q = match[1].replace(/"/g, '\\"');
+
+    if (q.trim().length > 1) {
+      debounce(
+        request({
+          url: 'https://api.npms.io/v2/search',
+          qs: { q },
+          json: true
+        }).then(data => {
+          if (data.results.length < 1) {
+            display({
+              title: `No packages with query ${q} found`
+            });
+          } else {
+            data.results.forEach(item => {
+              display({
+                title: item.package.name,
+                subtitle: <Subtitle item={item} />,
+                getPreview: () => (
+                  <Preview item={item} actions={ actions } />
+                ),
+                onSelect: event => {
+                  actions.open(item.package.links.npm);
+                }
+              })
+            });
+          }
+        })
+      , 500);
+    }
   }
-  
+
 }
--- src/preview/index.js
+++ src/preview/index.js
@@ -1,15 +1,15 @@
 import React, { Component, PropTypes } from 'react';
-import Scores from './scores';
-import Flags from './flags';
+import Scores from '../Scores';
+import Flags from '../Flags';
+import ReactMarkdown from 'react-markdown';
 import styles from './styles.css'
 
 export default ({ item, actions }) => (
   <div className={styles.main}>
     <h1>{item.package.name}</h1> <span className={styles.version}>({item.package.version})</span>
     <div><Flags flags={item.flags || {}} /></div>
-    <p>{item.package.description}</p>
-    
-    <Scores 
+    <ReactMarkdown source={item.package.description} />
+    <Scores
       popularity={Math.trunc(item.score.detail.popularity * 100)}
       maintenance={Math.trunc(item.score.detail.maintenance * 100)}
       quality={Math.trunc(item.score.detail.quality * 100)}
