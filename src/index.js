33dc394408ba72b888d222d21b0a9d5c1e4dfaa2
FreaKzero <pete@freakzero.com>
2017-11-24 14:30:14 +0100

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,43 +1,51 @@
-import debounce from 'debounce';
+import debounce from 'p-debounce';
 import request from 'request-promise-native';
 import React from 'react';
 import Preview from './Preview';
 import Subtitle from './Subtitle';
+import {memoize} from 'cerebro-tools';
 import styles from './Preview/styles.css';
+import icon from './assets/npms-logo.png';
 
-export const fn = ({ term, display, actions }) => {
+const mem = 60000;
+const deb = 500;
+
+const packagesRequest = (query) => request({
+      url: 'https://api.npms.io/v2/search',
+      qs: { q: query },
+      json: true
+    }).then(data => data.results);
+  
+
+const getPackages = debounce(memoize(packagesRequest, mem), deb);
+
+export const fn = ({ term, display, actions, hide }) => {
   let match = term.match(new RegExp(/^npms\s*(.*)/, 'i'));
 
   if (match && match[1]) {
-    let q = match[1].replace(/"/g, '\\"');
+    let query = match[1].replace(/"/g, '\\"');
 
-    if (q.trim().length > 1) {
-      debounce(
-        request({
-          url: 'https://api.npms.io/v2/search',
-          qs: { q },
-          json: true
-        }).then(data => {
-          if (data.results.length < 1) {
-            display({
-              title: `No packages with query ${q} found`
-            });
-          } else {
-            data.results.forEach(item => {
-              display({
-                title: item.package.name,
-                subtitle: <Subtitle item={item} />,
-                getPreview: () => (
-                  <Preview item={item} actions={ actions } />
-                ),
-                onSelect: event => {
-                  actions.open(item.package.links.npm);
-                }
-              })
-            });
-          }
-        })
-      , 500);
+    display({
+      icon,
+      id: 'npms-plugin-search',
+      title: `Searching npms for ${query}`
+    });
+
+    if (query.trim().length > 1) {
+      getPackages(query).then(data => {
+        hide('npms-plugin-search')
+        data.forEach(item => {
+          display({
+            icon,
+            title: item.package.name,
+            subtitle: <Subtitle item={item} />,
+            getPreview: () => <Preview item={item} actions={ actions } />,
+            onSelect: event => {
+              actions.open(item.package.links.npm);
+            }
+          })
+        });
+      });
     }
   }
 
