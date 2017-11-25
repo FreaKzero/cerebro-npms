de64264d2152a2c08223b92fd3ead6fa5a21937f
FreaKzero <pete@freakzero.com>
2017-11-25 03:11:02 +0100

    Remove example code

--- src/Links/index.js
+++ src/Links/index.js
@@ -1,21 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { KeyboardNav, KeyboardNavItem } from 'cerebro-ui'
 
-const Preview = ({ items, onSelect }) => (
-  <KeyboardNav>
-    <ul className={styles.list}>
-    {
-      items.map(s => (
-        <KeyboardNavItem key={s} onSelect={() => onSelect(item)}>
-          {item}
-        </KeyboardNavItem>
-      ))
-    }
-    </ul>
-  </KeyboardNav>
-)
-
-const openUrl = () => {}
 export default ({ item, actions }) => (
   <footer className='card-footer'>
     {Object.keys(item.package.links).map((name, idx) => 
