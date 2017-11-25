de7ce1a065ad8dc9bb19851482328588458253d5
FreaKzero <pete@freakzero.com>
2017-11-25 16:56:41 +0100

    Add tags

---
+++ src/Tags/index.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+import tagImg from '../assets/tag.png';
+import styles from './styles.css';
+
+export default ({ item, actions }) => (
+  <div className={styles.tags}>
+    {item.package.keywords.map(item => (
+      <span className='tag'>
+        <img 
+          src={tagImg} 
+          className={styles.tagIcon}
+        />
+        {item}
+      </span>
+    ))}
+  </div>
+)
--- src/preview/index.js
+++ src/preview/index.js
@@ -2,12 +2,14 @@ import React, { Component, PropTypes } from 'react';
 import Scores from '../Scores';
 import Flags from '../Flags';
 import Links from '../Links';
+import Tags from '../Tags';
 import ReactMarkdown from 'react-markdown';
 import styles from './styles.css'
 
 export default ({ item, actions }) => (
   <div className={styles.main}>
     <div className='card' tabIndex='1'>
+      
       <div className='card-header'>
         <span className='card-header-title'>
         <h3>{item.package.name}</h3> 
@@ -17,15 +19,13 @@ export default ({ item, actions }) => (
 
       <div className='card-content'>
         <div className='content'>
-        
-        <ReactMarkdown source={item.package.description} />
-
-        <Scores
-          popularity={Math.trunc(item.score.detail.popularity * 100)}
-          maintenance={Math.trunc(item.score.detail.maintenance * 100)}
-          quality={Math.trunc(item.score.detail.quality * 100)}
-        />
-
+          <ReactMarkdown source={item.package.description} />
+          <Tags item={item} />
+          <Scores
+            popularity={Math.trunc(item.score.detail.popularity * 100)}
+            maintenance={Math.trunc(item.score.detail.maintenance * 100)}
+            quality={Math.trunc(item.score.detail.quality * 100)}
+          />
         </div>
       </div>
       <Links item={item} actions={actions} />
